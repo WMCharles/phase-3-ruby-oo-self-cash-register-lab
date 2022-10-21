09f56ea5d19dd9a93ea2807656a7ba2ba5e84214
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-21 18:45:03 +0300

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,32 +1,36 @@
 class CashRegister
-    attr_accessor :total, :discount, :items, :last_transaction
-
-    def initialize(discount=0, total=0)
-        @total=total
-        @discount=discount
+    attr_accessor :discount, :last_transaction, :items, :total
+    def initialize(discount = 0, total = 0)
+        @discount = discount
+        @total = total
         @items = []
     end
 
-    def add_item(title, price, quantity=1)
-        self.total += price*quantity
+    def total 
+        return @total 
+    end
+
+    def add_item(title, price, quantity = 1)
+        quantity==1 ? self.items << title :  quantity.times{self.items << title}
         self.last_transaction = price*quantity
-        if quantity==1
-            self.items << title
-        else
-            quantity.times{self.items << title}
-        end
+        @total += price*quantity
     end
 
     def apply_discount
-        if self.discount==0
+        if @discount == 0
             return "There is no discount to apply."
         else
-            self.total -= self.total*(self.discount/100.to_f)
-            return "After the discount, the total comes to $#{self.total.to_i}."
+            @total -= (@total*@discount)/100
+            return "After the discount, the total comes to $#{@total}."
         end
     end
 
     def void_last_transaction
-        self.total -= self.last_transaction
+        @total -= self.last_transaction         
     end
 end
+
+p = CashRegister.new
+p p.add_item('cow', 90)
+p p.add_item('cow', 90)
+p.items
